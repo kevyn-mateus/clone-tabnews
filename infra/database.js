211afb186d1bef0eb6581dd2b411a427195175ed
kevyn-mateus <157160630+kevyn-mateus@users.noreply.github.com>
2024-05-29 02:45:54 +0000

    handles database ssl connection

--- infra/database.js
+++ infra/database.js
@@ -7,14 +7,15 @@ const getNewClient = async () => {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
-  console.log('Credencciais do Postgres:', {
-      host: process.env.POSTGRES_HOST,
-      port: process.env.POSTGRES_PORT,
-      user: process.env.POSTGRES_USER,
-      database: process.env.POSTGRES_DB,
-      password: process.env.POSTGRES_PASSWORD,
-    });
+  console.log("Credencciais do Postgres:", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
   await client.connect();
   return client;
 };
