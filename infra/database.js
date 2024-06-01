0a203bba6af3807bb1406aee63fb78781673fbd2
kevyn-mateus <157160630+kevyn-mateus@users.noreply.github.com>
2024-06-01 12:39:02 +0000

    add migration scripts

--- infra/database.js
+++ infra/database.js
@@ -9,13 +9,6 @@ const getNewClient = async () => {
     password: process.env.POSTGRES_PASSWORD,
     ssl: process.env.NODE_ENV === "development" ? false : true,
   });
-  console.log("Credencciais do Postgres:", {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-  });
   await client.connect();
   return client;
 };
